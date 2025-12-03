283a782cdbca927e65b34e3925af2b60e7fb2bba
Mauricio Pisco <mauriciopisco@gmail.com>
2025-12-03 11:35:15 -0300

    Add UserProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { useState } from "react";
 import { AppRoutes } from "./config/AppRoutes";
-import UserContext from "./context/UserContext";
-import type { User } from "./shared/types/User";
-import { getRandomUser } from "./shared/utils/randomData";
-
-let defaultUser: User = {
-  name: "jDev128",
-  avatar: "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/05/055874a3352a2b8bede001a85391b2c8594d7b71_full.jpg",
-};
+import { UserProvider } from "./context/UserContext";
 
 export const App = () => {
-
-  const [user, setUser] = useState(defaultUser);
-
-  const randomizeUser = () => {
-    setUser(getRandomUser());
-  }
-
 	return (
-		<UserContext value={{ user, randomizeUser }}>
+		<UserProvider>
 			<AppRoutes />
-		</UserContext>
+		</UserProvider>
 	);
 };
--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,18 +1,35 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, type ReactNode } from "react";
 import type { User } from "../shared/types/User";
+import { getRandomUser } from "../shared/utils/randomData";
 
-const initialValue: {user: User, randomizeUser: () => void} = {
-	user: {
-		name: "",
-		avatar: "",
-	},
-	randomizeUser: () => {}
+const defaultUser = {
+	name: "jDev128",
+	avatar: "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/05/055874a3352a2b8bede001a85391b2c8594d7b71_full.jpg",
+};
+
+const initialValue: { user: User; randomizeUser: () => void } = {
+	user: defaultUser,
+	randomizeUser: () => {},
 };
 
 const UserContext = createContext(initialValue);
 
+interface ProviderProps {
+	children: ReactNode;
+}
+
+export const UserProvider = ({ children }: ProviderProps) => {
+	const [user, setUser] = useState(defaultUser);
+
+	const randomizeUser = () => {
+		setUser(getRandomUser());
+	};
+
+	return (
+		<UserContext value={{ user, randomizeUser }}>{children}</UserContext>
+	);
+};
+
 export const useUserContext = () => {
 	return useContext(UserContext);
 };
-
-export default UserContext;
