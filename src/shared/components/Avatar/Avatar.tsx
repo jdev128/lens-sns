0dd597b27eda5483d300d05e3f3ce7c567ec8358
Mauricio Pisco <mauriciopisco@gmail.com>
2025-12-03 11:36:22 -0300

    Show default avatar on invalid urls

--- src/shared/components/Avatar/Avatar.tsx
+++ src/shared/components/Avatar/Avatar.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import styles from "./Avatar.module.css";
 import defaultAvatar from "/src/assets/octicon_feed-person-16.png";
 
@@ -12,10 +13,25 @@ export const Avatar = ({
 	size = "normal",
 	withBorder = false,
 }: Props) => {
+	const imageElement = useRef<HTMLImageElement>(null);
+
 	let classes = `${styles.avatar} ${
 		size === "normal" ? "" : styles.smallAvatar
 	} ${withBorder ? styles.highlightedAvatar : ""}`;
+
+	const handleLoadError = () => {
+		if (imageElement.current) {
+			imageElement.current.src = defaultAvatar;
+		}
+	};
+
 	return (
-		<img className={classes} src={imageURL ? imageURL : defaultAvatar} alt="" />
+		<img
+			className={classes}
+			src={imageURL}
+			alt=""
+			ref={imageElement}
+			onError={handleLoadError}
+		/>
 	);
 };
